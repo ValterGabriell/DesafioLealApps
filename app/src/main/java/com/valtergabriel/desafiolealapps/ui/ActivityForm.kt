83f123c056e1f691149a8c130fd08d6d55c6b98f
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2023-01-20 12:00:56 -0300

    criando tela de feed

---
+++ app/src/main/java/com/valtergabriel/desafiolealapps/ui/ActivityForm.kt
@@ -0,0 +1,17 @@
+package com.valtergabriel.desafiolealapps.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.valtergabriel.desafiolealapps.databinding.ActivityFormBinding
+
+class ActivityForm : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
--- app/src/main/java/com/valtergabriel/desafiolealapps/ui/MainActivity.kt
+++ app/src/main/java/com/valtergabriel/desafiolealapps/ui/MainActivity.kt
@@ -1,16 +1,26 @@
-package com.valtergabriel.desafiolealapps
+package com.valtergabriel.desafiolealapps.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.valtergabriel.desafiolealapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSignUp.setOnClickListener {
+            Intent(this, ActivityForm::class.java).also {
+                startActivity(it)
+            }
+        }
+
     }
+
+
 }
