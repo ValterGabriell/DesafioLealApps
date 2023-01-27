aa6a00a3ea48bcb1e24a7a0ff432a4bcebc31d4f
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2023-01-27 12:10:09 -0300

    reajustando pequenos bugs

--- app/src/main/java/com/valtergabriel/desafiolealapps/dto/MockExercise.kt
+++ app/src/main/java/com/valtergabriel/desafiolealapps/dto/MockExercise.kt
@@ -15,7 +15,7 @@ data class MockExercise(
                 "Flexão de braço",
                 "Deitar de barriga para baixo, levantar o corpo esticando os braços alinhados ao corpo, em uma distância um pouco maior que a largura dos ombros, os pés no chão,  abdômen contraído e costas alinhadas. Levantar e abaixar o corpo apenas dobrando e esticando os braços em um ângulo de 90º com o cotovelo",
                 "BICEPES",
-                "0"
+                ""
             )
         )
         list.add(
@@ -24,7 +24,7 @@ data class MockExercise(
                 "Flexão de braço inclinada",
                 "pegar um uma superfície para criar a inclinação do seu corpo como Kettle, banquinho, cadeira, puff, bola de ginástica ou uma plataforma de exercício de step. Apoiar as mãos na superfície inclinada, com os braços alinhados ao corpo, em uma distância um pouco maior que a largura dos ombros e os pés no chão. O corpo deve reto com as costas alinhadas com o tronco. Contrair o abdômen, flexionar os cotovelos até que o peito encoste na superfície e retornar à posição inicial. Pode-se fazer 2 a 3 séries de 8 a 10 repetições cada, descansando de 60 a 90 segundos entre as séries.",
                 "BICEPES",
-                "0"
+                ""
             )
         )
         return list
